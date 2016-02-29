91307628f23f987b3bd3e7c6c2d7e8283a3af9ca
Seefo <rezen1911@gmail.com>
2016-02-29 18:37:50 -0500

    Added missing files

---
+++ Generator/Constraints/ConstraintSet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RTFP.Generator.Constraints;
+
+namespace RTFP.Generator.Constraints
+{
+	public class ConstraintSet
+	{
+		private Dictionary<string, Constraint> Constraints { get; set; }
+
+		public ConstraintSet()
+		{
+			this.Constraints = new Dictionary<string, Constraint>();
+		}
+
+		public void Add(string name, Constraint constraint)
+		{
+			Constraints[name] = constraint;
+		}
+
+		public void Remove(string name)
+		{ 
+			Constraints.Remove(name);
+		}
+
+		public Constraint Get(string constraintName)
+		{
+			return Constraints[constraintName];
+		}
+
+		public object GenerateValue(string constraintName)
+		{
+			return Get(constraintName).ProduceValidValue();
+		}
+
+		public bool HasConstraint(string constraintName)
+		{
+			return Constraints.Keys.Contains(constraintName);
+		}		
+	}
+}
---
+++ Generator/Generator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RTFP.Generator.Constraints;
+
+namespace RTFP.Generator
+{
+	interface Generator
+	{
+		ConstraintSet Constraints { get; set; }
+	}
+}
