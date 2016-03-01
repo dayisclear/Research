a0290c20319cbd2237e1b527bd770f5b04c70a8c
Seefo <rezen1911@gmail.com>
2016-03-01 13:13:16 -0500

    Preparing new FloorPlan structure

--- Generator/FloorPlan/FloorPlan.cs
+++ Generator/FloorPlan/FloorPlan.cs
@@ -12,7 +12,25 @@ namespace RTFP.Generator.FloorPlan
 {
 	public class FloorPlan
 	{
+		class Room
+		{
+			public int X, Y, Width, Height;
+			public RoomType Type;
+		}
+
+		private List<Room> Rooms { get; set; }
 		public List<Edge> Edges { get; set; }
 		public List<Vertex> Vertices { get; set; }
+
+		public void AddRoom(int x, int y, int width, int height, RoomType type)
+		{
+			Rooms.Add(new Room() { X = x, Y = y, Width = width, Height = height, Type = type });
+		}
+
+		public void MergeFloorPlan(FloorPlan fp)
+		{
+			foreach (Room r in fp.Rooms)
+				this.Rooms.Add(r);
+		}
 	}
 }
