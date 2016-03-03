a0c19d18d73594adafb0ef9c93d1e43cf36da1d5
Seefo <rezen1911@gmail.com>
2016-03-03 13:26:03 -0500

    Added primitive MST algorithm

---
+++ Algorithms/MST/Prims.cs
@@ -0,0 +1,55 @@
+﻿using RTFP.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTFP.Algorithms.MST
+{
+	public static class Prims
+	{
+		public static bool FindMST(int source, int[,] matrix)
+		{
+			List<int> mst = new List<int>();
+			mst.Add(source);
+
+			// While we haven't added all our nodes to the mst
+			while(mst.Count < matrix.Length)
+			{
+				int from = Int32.MinValue;
+				int min = Int32.MaxValue;
+
+				// Iterate across current mst nodes and find cheapest edge
+				for(int i = 0; i < mst.Count; i++)
+				{
+					// Iterate across mst node's connections looking for a better min
+					for (int j = 0; j < matrix.Length; j++)
+					{
+						int current = mst[i];
+
+						// Make sure edge is better than min and connected
+						if (matrix[current, j] < min && matrix[current, j] > 0)
+						{
+							// Make sure we haven't already visited the node
+							if (!mst.Contains(matrix[current, j]))
+							{
+								min = i;
+								from = mst[i];
+							}
+						}
+					}
+				}
+
+				// If we successfully found a cheap path lets add it to the mst
+				if(min != Int32.MaxValue && from != Int32.MinValue)
+				{
+					mst.Add(min);
+				}
+				else return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ DataStructures/Graph.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTFP.DataStructures
+{
+	public class Graph
+	{
+		private class Edge
+		{
+			public Object Source, Destination;
+			public int Cost;
+			public Edge(ref Object src, ref Object dst, int cost)
+			{
+				this.Source = src;
+				this.Destination = dst;
+				this.Cost = cost;
+			}
+		}
+
+		List<Object> Nodes = new List<Object>();
+		List<Edge> Edges = new List<Edge>();
+
+		public int[,] AdjacencyMatrix
+		{
+			get
+			{
+				int[,] matrix = new int[Nodes.Count, Nodes.Count];
+
+				foreach (Edge e in Edges)
+				{
+					matrix[Nodes.IndexOf(e.Source), Nodes.IndexOf(e.Destination)] = e.Cost;
+					matrix[Nodes.IndexOf(e.Destination), Nodes.IndexOf(e.Source)] = e.Cost;
+				}
+
+				return matrix;
+			}
+		}
+
+		public void AddNode(ref Object Object)
+		{
+			Nodes.Add(Object);
+		}
+
+		public void AddEdge(ref Object from, ref Object to, int cost)
+		{
+			Edges.Add(new Edge(ref from, ref to, cost));
+		}
+	}
+}
