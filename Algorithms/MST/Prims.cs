617e6fb62ea74904d650069e8e81246bf71ff92c
Seefo <rezen1911@gmail.com>
2016-03-08 01:07:07 -0500

    Further cleaned up code, reverted main method

--- Algorithms/MST/Prims.cs
+++ Algorithms/MST/Prims.cs
@@ -65,5 +65,58 @@ namespace RTFP.Algorithms.MST
 
 			return mst;
 		}
+
+		public static void TestPrim()
+		{
+			Graph tree = new Graph();
+
+			Object a = "a";
+			Object b = "b";
+			Object c = "c";
+			Object d = "d";
+			Object e = "e";
+			Object f = "f";
+			Object g = "g";
+
+			tree.AddEdge(a, b, 2);
+			tree.AddEdge(a, c, 3);
+			tree.AddEdge(a, d, 3);
+
+			tree.AddEdge(b, a, 2);
+			tree.AddEdge(b, c, 4);
+			tree.AddEdge(b, e, 3);
+
+			tree.AddEdge(c, a, 3);
+			tree.AddEdge(c, b, 4);
+			tree.AddEdge(c, e, 1);
+			tree.AddEdge(c, d, 5);
+			tree.AddEdge(c, f, 6);
+
+			tree.AddEdge(d, a, 3);
+			tree.AddEdge(d, c, 5);
+			tree.AddEdge(d, f, 7);
+
+			tree.AddEdge(f, d, 7);
+			tree.AddEdge(f, c, 6);
+			tree.AddEdge(f, e, 8);
+
+			tree.AddEdge(f, g, 9);
+
+			var mst = Algorithms.MST.Prims.FindMST(a, tree);
+
+			foreach (var n in mst) Console.WriteLine(n);
+
+			/*
+
+			Expected output:
+				b -> a
+				c -> a
+				c -> e
+				d -> a
+				f -> c
+				f -> g
+
+			*/
+		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -16,52 +16,13 @@ namespace RTFP
 	{
 		static void Main()
 		{
-			Graph tree = new Graph();
-
-			Object a = "a";
-			Object b = "b";
-			Object c = "c";
-			Object d = "d";
-			Object e = "e";
-			Object f = "f";
-			Object g = "g";
-
-			tree.AddEdge( a,  b, 2);
-			tree.AddEdge( a,  c, 3);
-			tree.AddEdge( a,  d, 3);
-
-			tree.AddEdge( b,  a, 2);
-			tree.AddEdge( b,  c, 4);
-			tree.AddEdge( b,  e, 3);
-
-			tree.AddEdge( c,  a, 3);
-			tree.AddEdge( c,  b, 4);
-			tree.AddEdge( c,  e, 1);
-			tree.AddEdge( c,  d, 5);
-			tree.AddEdge( c,  f, 6);
-
-			tree.AddEdge( d,  a, 3);
-			tree.AddEdge( d,  c, 5);
-			tree.AddEdge( d,  f, 7);
-
-			tree.AddEdge( f,  d, 7);
-			tree.AddEdge( f,  c, 6);
-			tree.AddEdge( f,  e, 8);
-
-			tree.AddEdge( f,  g, 9);
-
-			var mst = Algorithms.MST.Prims.FindMST( a, tree);
-			foreach (var n in mst) Console.WriteLine(n);
-			Console.Read();
-
-
-		/*	SuburbanGenerator generator = new SuburbanGenerator();
+			SuburbanGenerator generator = new SuburbanGenerator();
 
 			while (true)
 			{
 				FloorPlan fp = generator.GenerateFloorPlan();
 				DrawFloorPlan(fp);
-			}*/
+			}
 		}
 
 		private static void DrawFloorPlan(FloorPlan fp)
